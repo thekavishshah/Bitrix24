8e9af791c21b321eedcc3140d3ea3e4e42725a3b
Rahul Gupta <rg5353070@gmail.com>
2024-12-04 22:58:03 +0530

    fixed changes to raw and inferred deals

--- app/(protected)/raw-deals/page.tsx
+++ app/(protected)/raw-deals/page.tsx
@@ -1,31 +1,7 @@
-import DealCardSkeleton from "@/components/skeletons/DealCardSkeleton";
-import { Metadata } from "next";
-import React, { Suspense } from "react";
-import FetchingRawDeals from "./FetchingRawDeals";
+import React from "react";
 
-export const metadata: Metadata = {
-  title: "Raw Deals",
-  description: "View the raw deals scraped from listings site",
+const page = () => {
+  return <div>page</div>;
 };
 
-const RawDealsPage = async () => {
-  return (
-    <div className="block-space container">
-      <h1 className="mb-4 text-center md:mb-6 lg:mb-8">Raw Deals</h1>
-      <Suspense
-        fallback={
-          <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-            <DealCardSkeleton />
-            <DealCardSkeleton />
-            <DealCardSkeleton />
-            <DealCardSkeleton />
-            <DealCardSkeleton />
-            <DealCardSkeleton />
-          </div>
-        }
-      ></Suspense>
-    </div>
-  );
-};
-
-export default RawDealsPage;
+export default page;
