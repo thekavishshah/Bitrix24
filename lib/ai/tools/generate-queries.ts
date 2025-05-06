bddd8b27a5fe198c911e1eb32313ceff2f1bccb5
Rahul Gupta <rg5353070@gmail.com>
2025-05-06 14:44:29 +0530

    fixed tools error

--- lib/ai/tools/generate-queries.ts
+++ lib/ai/tools/generate-queries.ts
@@ -1,13 +1,13 @@
 import { z } from "zod";
 
 import { generateObject } from "ai";
-import { mainAIModel } from "../available-models";
+import { openai } from "../available-models";
 
 const generateSearchQueries = async (query: string, n: number = 3) => {
   const {
     object: { queries },
   } = await generateObject({
-    model: mainAIModel,
+    model: openai("gpt-4o"),
     prompt: `Generate ${n} search queries for the following query: ${query}`,
     schema: z.object({
       queries: z.array(z.string()).min(1).max(5),
